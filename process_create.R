693e7c9e387d94fe6bc6c2613abf1b7edc1f2cea
sunitgk <skenner@extensis.com>
2019-01-22 13:06:30 -0800

    Commit blog sunitakener

---
+++ process_create.R
@@ -0,0 +1,9 @@
+library(blogdown)
+new_site(theme = "jpescador/hugo-future-imperfect",
+         sample = TRUE,
+         theme_example = TRUE,
+         empty_dirs = TRUE,
+         to_yaml = TRUE)
+
+library(usethis)
+edit_r_profile()
